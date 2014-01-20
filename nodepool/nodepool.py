a84540f33214eb31228e964bb193402903568754
Robert Collins <rbtcollins@hp.com>
2014-01-21 12:03:08 +1300

    Cleanup nodes in state DELETE immediately.
    The 900 second delay before deleting them from periodic cleanup just
    causes excessive waste when we've restarted nodepool.
    
    To avoid concurrent sqlalchemy access (particularly mutations) to node
    objects conditions with other per-node operations, this delegates all
    deletions to periodic cleanup.
    
    This is done because with the high latency on API command submission
    due to low rate limits with providers (e.g. 2 requests/second) it's
    likely that a node deleted during busy periods will not have it's API
    request *submitted* for up to 5 minutes. As such, the 5 minute cleanup
    thread will actually submit the API requests with little or no
    increase in latency. And of course that frequency could be increased,
    permitting faster cleanup in low-load situations.
    
    Change-Id: Iad7004b0d3389df1e1b687209fd5f6ed27b03239

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -124,7 +124,8 @@ class NodeCompleteThread(threading.Thread):
             statsd.incr(key + '.builds')
 
         time.sleep(DELETE_DELAY)
-        self.nodepool.deleteNode(session, node)
+        # Mark the node for deletion by the provider.
+        node.state = nodedb.DELETE
 
 
 class NodeUpdateListener(threading.Thread):
@@ -282,10 +283,10 @@ class NodeLauncher(threading.Thread):
                 self.log.exception("Exception launching node id: %s:" %
                                    self.node_id)
                 try:
-                    self.nodepool.deleteNode(session, self.node)
+                    self.node.state = nodedb.DELETE
                 except Exception:
-                    self.log.exception("Exception deleting node id: %s:" %
-                                       self.node_id)
+                    self.log.exception("Exception marking node id: %s "
+                                       "for deletion" % self.node_id)
                     return
 
     def launchNode(self, session):
@@ -1181,7 +1182,8 @@ class NodePool(threading.Thread):
     def cleanupOneNode(self, session, node):
         now = time.time()
         time_in_state = now - node.state_time
-        if (node.state in [nodedb.READY, nodedb.HOLD] or
+        if node.state != nodedb.DELETE and (
+            node.state in [nodedb.READY, nodedb.HOLD] or
             time_in_state < 900):
             return
         delete = False
@@ -1265,7 +1267,7 @@ class NodePool(threading.Thread):
             except Exception:
                 self.log.exception("SSH Check failed for node id: %s" %
                                    node.id)
-                self.deleteNode(session, node)
+                node.state = nodedb.DELETE
         self.log.debug("Finished periodic check")
 
     def updateStats(self, session, provider_name):
