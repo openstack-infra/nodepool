62deb91500f70da51869c4c44c8be1b63f8c7ebb
Brad P. Crochet <brad@redhat.com>
2014-05-21 11:07:10 -0400

    Add cloud-user to non-root user list
    RHEL and CentOS cloud images use 'cloud-user' as the default user for
    login. This adds support for trying that user if root does not work.
    
    Change-Id: Ie9e4276800408dee5941971d49da35252f9a74ee

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -824,7 +824,7 @@ class ImageUpdater(threading.Thread):
             # We have connected to the node but couldn't do anything as root
             # try distro specific users, since we know ssh is up (a timeout
             # didn't occur), we can connect with a very sort timeout.
-            for username in ['ubuntu', 'fedora']:
+            for username in ['ubuntu', 'fedora', 'cloud-user']:
                 try:
                     host = utils.ssh_connect(server['public_v4'], username,
                                              ssh_kwargs,
