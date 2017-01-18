7274778c113ae4037d03e2cece5fb94d529998bb
James E. Blair <jeblair@redhat.com>
2017-01-18 14:16:58 -0800

    Add files for zuul-nodepool integration test
    This adds config files and a startup script.
    
    Remove the nodepool.yaml .gitignore.
    
    Change-Id: Icc58521f520f719f24f59132c424b3a71432285f

---
+++ tools/zuul-nodepool-integration/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")"
+
+mkdir -p /tmp/nodepool/images
+mkdir -p /tmp/nodepool/log
+
+nodepool-builder -c `pwd`/nodepool.yaml -l `pwd`/builder-logging.conf -p /tmp/nodepool/builder.pid --fake
+nodepoold -c `pwd`/nodepool.yaml -s `pwd`/secure.conf -l `pwd`/launcher-logging.conf -p /tmp/nodepool/launcher.pid
