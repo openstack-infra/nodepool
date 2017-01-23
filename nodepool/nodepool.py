5ee68af0014f5abfadcfefc8a6e8057acf365347
David Shrewsbury <shrewsbury.dave@gmail.com>
2017-01-23 14:28:47 -0500

    Add nodepoold test for request decline and fail
    Add a test that validates that the node request is declined
    and then set to failed if it cannot be satisfied. We use an
    invalid image name in the test to get to the proper code path.
    
    Change-Id: I860d16740ac3cac06ae5aedafc7409159d50a566

--- nodepool/nodepool.py
+++ nodepool/nodepool.py
@@ -800,7 +800,8 @@ class ProviderWorker(threading.Thread):
     that will be recognized and this thread will shut itself down.
     '''
 
-    def __init__(self, configfile, zk, provider):
+    def __init__(self, configfile, zk, provider,
+                 watermark_sleep=WATERMARK_SLEEP):
         threading.Thread.__init__(
             self, name='ProviderWorker.%s' % provider.name
         )
@@ -810,6 +811,7 @@ class ProviderWorker(threading.Thread):
         self.running = False
         self.configfile = configfile
         self.workers = []
+        self.watermark_sleep = watermark_sleep
         self.launcher_id = "%s-%s-%s" % (socket.gethostname(),
                                          os.getpid(),
                                          self.ident)
@@ -913,7 +915,7 @@ class ProviderWorker(threading.Thread):
             if self.provider.max_concurrency != 0:
                 self._processRequests()
 
-            time.sleep(10)
+            time.sleep(self.watermark_sleep)
             self._updateProvider()
 
     def stop(self):
@@ -936,6 +938,7 @@ class NodePool(threading.Thread):
         self.apsched = None
         self.zk = None
         self.statsd = stats.get_client()
+        self._provider_threads = {}
         self._delete_threads = {}
         self._delete_threads_lock = threading.Lock()
         self._instance_delete_threads = {}
@@ -951,7 +954,17 @@ class NodePool(threading.Thread):
             provider_manager.ProviderManager.stopProviders(self.config)
         if self.apsched and self.apsched.running:
             self.apsched.shutdown()
-        self.log.debug("finished stopping")
+
+        # Don't let stop() return until all provider threads have been
+        # terminated.
+        self.log.debug("Stopping provider threads")
+        for thd in self._provider_threads.values():
+            if thd.isAlive():
+                thd.stop()
+            self.log.debug("Waiting for %s" % thd.name)
+            thd.join()
+
+        self.log.debug("Finished stopping")
 
     def loadConfig(self):
         self.log.debug("Loading configuration")
@@ -1243,9 +1256,6 @@ class NodePool(threading.Thread):
         '''
         Start point for the NodePool thread.
         '''
-        # Provider threads keyed by provider name
-        provider_threads = {}
-
         while not self._stopped:
             try:
                 self.updateConfig()
@@ -1259,30 +1269,25 @@ class NodePool(threading.Thread):
                 # the config. Removing a provider from the config and then
                 # adding it back would cause a restart.
                 for p in self.config.providers.values():
-                    if p.name not in provider_threads.keys():
-                        t = ProviderWorker(self.configfile, self.zk, p)
+                    if p.name not in self._provider_threads.keys():
+                        t = ProviderWorker(self.configfile, self.zk, p,
+                                           self.watermark_sleep)
                         self.log.info( "Starting %s" % t.name)
                         t.start()
-                        provider_threads[p.name] = t
-                    elif not provider_threads[p.name].isAlive():
-                        provider_threads[p.name].join()
-                        t = ProviderWorker(self.configfile, self.zk, p)
+                        self._provider_threads[p.name] = t
+                    elif not self._provider_threads[p.name].isAlive():
+                        self._provider_threads[p.name].join()
+                        t = ProviderWorker(self.configfile, self.zk, p,
+                                           self.watermark_sleep)
                         self.log.info( "Restarting %s" % t.name)
                         t.start()
-                        provider_threads[p.name] = t
+                        self._provider_threads[p.name] = t
             except Exception:
                 self.log.exception("Exception in main loop:")
 
             self._wake_condition.acquire()
             self._wake_condition.wait(self.watermark_sleep)
             self._wake_condition.release()
-
-        # Stop provider threads
-        for thd in provider_threads.values():
-            if thd.isAlive():
-                thd.stop()
-            self.log.info("Waiting for %s" % thd.name)
-            thd.join()
 
     def _run(self, session, allocation_history):
         # Make up the subnode deficit first to make sure that an
