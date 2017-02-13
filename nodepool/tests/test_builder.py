b5b3ad84d03c1102c2c1e60658bf77e7e060979a
Paul Belanger <pabelanger@redhat.com>
2017-02-13 16:01:31 -0500

    Re-enable test_image_upload_fail test
    Update test to use new zookeeper syntax.
    
    Change-Id: I2ace2f1f9e0827ab391821094674973d75e8b556
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- nodepool/tests/test_builder.py
+++ nodepool/tests/test_builder.py
@@ -15,7 +15,6 @@
 
 import os
 import fixtures
-from unittest import skip
 
 from nodepool import builder, exceptions, fakeprovider, tests
 from nodepool import zk
@@ -96,7 +95,6 @@ class TestNodePoolBuilder(tests.DBTestCase):
         nb.start()
         nb.stop()
 
-    @skip("Disabled for early v3 development")
     def test_image_upload_fail(self):
         """Test that image upload fails are handled properly."""
 
@@ -118,7 +116,8 @@ class TestNodePoolBuilder(tests.DBTestCase):
         self._useBuilder(configfile)
         pool.start()
         self.waitForImage('fake-provider', 'fake-image')
-        self.waitForNodes(pool)
+        nodes = self.waitForNodes('fake-label')
+        self.assertEqual(len(nodes), 1)
 
         newest_builds = self.zk.getMostRecentBuilds(1, 'fake-image',
                                                     state=zk.READY)
