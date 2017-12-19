aa16b8b8911fdaa07a68ad933de9fb4d07461134
Tristan Cacqueray <tdecacqu@redhat.com>
2017-12-19 06:10:26 +0000

    Amazon EC2 driver
    This change adds an experimental AWS driver. It lacks some of the deeper
    features of the openstack driver, such as quota management and image
    building, but is highly functional for running tests on a static AMI.
    
    Note that the test base had to be refactored to allow fixtures to be
    customized in a more flexible way.
    
    Change-Id: I313f9da435dfeb35591e37ad0bec921c8b5bc2b5
    Co-Authored-By: Tristan Cacqueray <tdecacqu@redhat.com>
    Co-Authored-By: David Moreau-Simard <dmsimard@redhat.com>
    Co-AUthored-By: Clint Byrum <clint@fewbar.com>

---
+++ nodepool/driver/aws/handler.py
@@ -0,0 +1,157 @@
+# Copyright 2018 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import time
+
+from nodepool import exceptions
+from nodepool import zk
+from nodepool.driver.utils import NodeLauncher
+from nodepool.driver import NodeRequestHandler
+from nodepool.nodeutils import nodescan
+
+
+class AwsInstanceLauncher(NodeLauncher):
+    def __init__(self, handler, node, provider_config, provider_label):
+        super().__init__(handler.zk, node, provider_config)
+        self.retries = provider_config.launch_retries
+        self.pool = provider_config.pools[provider_label.pool.name]
+        self.handler = handler
+        self.zk = handler.zk
+        self.boot_timeout = provider_config.boot_timeout
+        self.label = provider_label
+
+    def launch(self):
+        self.log.debug("Starting %s instance" % self.node.type)
+        attempts = 1
+        while attempts <= self.retries:
+            try:
+                instance = self.handler.manager.createInstance(self.label)
+                break
+            except Exception:
+                if attempts <= self.retries:
+                    self.log.exception(
+                        "Launch attempt %d/%d failed for node %s:",
+                        attempts, self.retries, self.node.id)
+                if attempts == self.retries:
+                    raise
+                attempts += 1
+            time.sleep(1)
+
+        instance.create_tags(Tags=[{'Key': 'nodepool_id',
+                                    'Value': str(self.node.id)}])
+        instance_id = instance.id
+        self.node.external_id = instance_id
+        self.zk.storeNode(self.node)
+
+        boot_start = time.monotonic()
+        while time.monotonic() - boot_start < self.boot_timeout:
+            state = instance.state.get('Name')
+            self.log.debug("Instance %s is %s" % (instance_id, state))
+            if state == 'running':
+                break
+            time.sleep(0.5)
+            instance.reload()
+        if state != 'running':
+            raise exceptions.LaunchStatusException(
+                "Instance %s failed to start: %s" % (instance_id, state))
+
+        server_ip = instance.public_ip_address
+        if not server_ip:
+            raise exceptions.LaunchStatusException(
+                "Instance %s doesn't have a public ip" % instance_id)
+
+        self.node.connection_port = self.label.cloud_image.connection_port
+        self.node.connection_type = self.label.cloud_image.connection_type
+        if self.pool.host_key_checking:
+            try:
+                if self.node.connection_type == 'ssh':
+                    gather_hostkeys = True
+                else:
+                    gather_hostkeys = False
+                keys = nodescan(server_ip, port=self.node.connection_port,
+                                timeout=180, gather_hostkeys=gather_hostkeys)
+            except Exception:
+                raise exceptions.LaunchKeyscanException(
+                    "Can't scan instance %s key" % instance_id)
+
+        self.log.info("Instance %s ready" % instance_id)
+        self.node.state = zk.READY
+        self.node.external_id = instance_id
+        self.node.hostname = server_ip
+        self.node.interface_ip = server_ip
+        self.node.public_ipv4 = server_ip
+        self.node.host_keys = keys
+        self.node.username = self.label.cloud_image.username
+        self.zk.storeNode(self.node)
+        self.log.info("Instance %s is ready", instance_id)
+
+
+class AwsNodeRequestHandler(NodeRequestHandler):
+    log = logging.getLogger("nodepool.driver.aws."
+                            "AwsNodeRequestHandler")
+
+    def __init__(self, pw, request):
+        super().__init__(pw, request)
+        self._threads = []
+
+    @property
+    def alive_thread_count(self):
+        count = 0
+        for t in self._threads:
+            if t.isAlive():
+                count += 1
+        return count
+
+    def imagesAvailable(self):
+        '''
+        Determines if the requested images are available for this provider.
+
+        :returns: True if it is available, False otherwise.
+        '''
+        if self.provider.manage_images:
+            for label in self.request.node_types:
+                if self.pool.labels[label].cloud_image:
+                    if not self.manager.labelReady(self.pool.labels[label]):
+                        return False
+        return True
+
+    def launchesComplete(self):
+        '''
+        Check if all launch requests have completed.
+
+        When all of the Node objects have reached a final state (READY or
+        FAILED), we'll know all threads have finished the launch process.
+        '''
+        if not self._threads:
+            return True
+
+        # Give the NodeLaunch threads time to finish.
+        if self.alive_thread_count:
+            return False
+
+        node_states = [node.state for node in self.nodeset]
+
+        # NOTE: It very important that NodeLauncher always sets one of
+        # these states, no matter what.
+        if not all(s in (zk.READY, zk.FAILED) for s in node_states):
+            return False
+
+        return True
+
+    def launch(self, node):
+        label = self.pool.labels[node.type[0]]
+        thd = AwsInstanceLauncher(self, node, self.provider, label)
+        thd.start()
+        self._threads.append(thd)
---
+++ nodepool/driver/aws/provider.py
@@ -0,0 +1,151 @@
+# Copyright 2018 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import boto3
+
+from nodepool.driver import Provider
+from nodepool.driver.aws.handler import AwsNodeRequestHandler
+
+
+class AwsInstance:
+    def __init__(self, name, metadatas, provider):
+        self.id = name
+        self.name = name
+        self.metadata = {}
+        if metadatas:
+            for metadata in metadatas:
+                if metadata["Key"] == "nodepool_id":
+                    self.metadata = {
+                        'nodepool_provider_name': provider.name,
+                        'nodepool_node_id': metadata["Value"],
+                    }
+                    break
+
+    def get(self, name, default=None):
+        return getattr(self, name, default)
+
+
+class AwsProvider(Provider):
+    log = logging.getLogger("nodepool.driver.aws.AwsProvider")
+
+    def __init__(self, provider, *args):
+        self.provider = provider
+        self.ec2 = None
+
+    def getRequestHandler(self, poolworker, request):
+        return AwsNodeRequestHandler(poolworker, request)
+
+    def start(self, zk_conn):
+        if self.ec2 is not None:
+            return True
+        self.log.debug("Starting")
+        self.aws = boto3.Session(
+            region_name=self.provider.region_name,
+            profile_name=self.provider.profile_name)
+        self.ec2 = self.aws.resource('ec2')
+
+    def stop(self):
+        self.log.debug("Stopping")
+
+    def listNodes(self):
+        servers = []
+
+        for instance in self.ec2.instances.all():
+            if instance.state["Name"].lower() == "terminated":
+                continue
+            servers.append(AwsInstance(
+                instance.id, instance.tags, self.provider))
+        return servers
+
+    def getImage(self, image_id):
+        return self.ec2.Image(image_id)
+
+    def labelReady(self, label):
+        if not label.cloud_image:
+            msg = "A cloud-image (AMI) must be supplied with the AWS driver."
+            raise Exception(msg)
+
+        image = self.getImage(label.cloud_image.external_name)
+        # Image loading is deferred, check if it's really there
+        if image.state != 'available':
+            self.log.warning(
+                "Provider %s is configured to use %s as the AMI for"
+                " label %s and that AMI is there but unavailable in the"
+                " cloud." % (self.provider.name,
+                             label.cloud_image.external_name,
+                             label.name))
+            return False
+        return True
+
+    def join(self):
+        return True
+
+    def cleanupLeakedResources(self):
+        # TODO: remove leaked resources if any
+        pass
+
+    def cleanupNode(self, server_id):
+        if self.ec2 is None:
+            return False
+        instance = self.ec2.Instance(server_id)
+        instance.terminate()
+
+    def waitForNodeCleanup(self, server_id):
+        # TODO: track instance deletion
+        return True
+
+    def createInstance(self, label):
+        image_name = label.cloud_image.external_name
+        args = dict(
+            ImageId=image_name,
+            MinCount=1,
+            MaxCount=1,
+            KeyName=label.key_name,
+            InstanceType=label.flavor_name,
+            NetworkInterfaces=[{
+                'AssociatePublicIpAddress': True,
+                'DeviceIndex': 0}])
+
+        if label.pool.security_group_id:
+            args['NetworkInterfaces'][0]['Groups'] = [
+                label.pool.security_group_id
+            ]
+        if label.pool.subnet_id:
+            args['NetworkInterfaces'][0]['SubnetId'] = label.pool.subnet_id
+
+        # Default block device mapping parameters are embedded in AMIs.
+        # We might need to supply our own mapping before lauching the instance.
+        # We basically want to make sure DeleteOnTermination is true and be
+        # able to set the volume type and size.
+        image = self.getImage(image_name)
+        # TODO: Flavors can also influence whether or not the VM spawns with a
+        # volume -- we basically need to ensure DeleteOnTermination is true
+        if hasattr(image, 'block_device_mappings'):
+            bdm = image.block_device_mappings
+            mapping = bdm[0]
+            if 'Ebs' in mapping:
+                mapping['Ebs']['DeleteOnTermination'] = True
+                if label.volume_size:
+                    mapping['Ebs']['VolumeSize'] = label.volume_size
+                if label.volume_type:
+                    mapping['Ebs']['VolumeType'] = label.volume_type
+                # If the AMI is a snapshot, we cannot supply an "encrypted"
+                # parameter
+                if 'Encrypted' in mapping['Ebs']:
+                    del mapping['Ebs']['Encrypted']
+                args['BlockDeviceMappings'] = [mapping]
+
+        instances = self.ec2.create_instances(**args)
+        return self.ec2.Instance(instances[0].id)
