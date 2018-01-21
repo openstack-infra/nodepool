4c533c8718d0f7ee149ca9d5b31e69a5935602ba
Paul Belanger <pabelanger@redhat.com>
2018-01-21 14:19:01 -0500

    Drop python2 virtualenv for devstack
    We no longer support python2, so we can just test using python3
    virtualenv.
    
    Change-Id: Id01f470e2d3424c3789a8f05f9eb05ac783f8363
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -61,11 +61,7 @@ function install_glean {
 
 # Install nodepool code
 function install_nodepool {
-    if python3_enabled; then
-        VENV="virtualenv -p python${PYTHON3_VERSION}"
-    else
-        VENV="virtualenv -p python${PYTHON2_VERSION}"
-    fi
+    VENV="virtualenv -p python3"
     $VENV $NODEPOOL_INSTALL
     install_shade
     install_diskimage_builder
