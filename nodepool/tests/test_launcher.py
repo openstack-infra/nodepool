cdd60504ecdfb97a3f85dffdb7ad18aedbf2021d
David Shrewsbury <shrewsbury.dave@gmail.com>
2018-10-11 17:38:31 -0400

    Cleanup down ports
    Cleanup will be periodic (every 3 minutes by default, not yet
    configurable) and will be logged and reported via statsd.
    
    Change-Id: I81b57d6f6142e64dd0ebf31531ca6489d6c46583

--- nodepool/tests/test_launcher.py
+++ nodepool/tests/test_launcher.py
@@ -1751,3 +1751,24 @@ class TestLauncher(tests.DBTestCase):
 
         req3 = self.waitForNodeRequest(req3)
         self.assertEqual(req3.state, zk.FAILED)
+
+    def test_leaked_port_cleanup(self):
+        configfile = self.setup_config('node.yaml')
+        self.useBuilder(configfile)
+        pool = self.useNodepool(configfile, watermark_sleep=1)
+        pool.cleanup_interval = 1
+        pool.start()
+        self.waitForNodes('fake-label')
+
+        manager = pool.getProviderManager('fake-provider')
+        down_ports = manager.listPorts(status='DOWN')
+        self.assertEqual(2, len(down_ports))
+        self.log.debug("Down ports: %s", down_ports)
+
+        # Change the port cleanup interval to happen quicker
+        manager._port_cleanup_interval_secs = 2
+        while manager.listPorts(status='DOWN'):
+            time.sleep(1)
+
+        self.assertReportedStat('nodepool.provider.fake-provider.downPorts',
+                                value='2', kind='c')
